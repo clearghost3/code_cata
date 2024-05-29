0df42c8573736fbe42d200755b12b32d07dee370
clearghost3 <165910897+clearghost3@users.noreply.github.com>
2024-05-29 10:29:24 +0900

    Create 44.js

---
+++ 44.js
@@ -0,0 +1,21 @@
+function solution(sizes) {
+    let big_num=0;
+    let min_num=0;
+    let x_n=0;
+    let y_n=0;
+    for (i=0;i<sizes.length;i++) {
+        if (big_num<=sizes[i][0]||big_num<=sizes[i][1]) {
+            big_num=sizes[i][0]>sizes[i][1] ? sizes[i][0]:sizes[i][1];
+        }
+        let temp=sizes[i][0]>sizes[i][1] ? sizes[i][1]:sizes[i][0];
+        if (min_num<temp) {
+            min_num=temp;
+        }
+    }
+    console.log(min_num);
+    
+    var answer = min_num*big_num;
+    return answer;
+}
+const size=[[60, 50], [30, 70], [60, 30], [80, 40]];
+console.log(solution(size));
