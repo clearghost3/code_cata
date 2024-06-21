848477ccba118c41a91284ee72a25367ea89134f
clearghost3 <165910897+clearghost3@users.noreply.github.com>
2024-06-21 11:37:35 +0900

    Create 68.js

---
+++ 68.js
@@ -0,0 +1,17 @@
+function solution(ingredient) {
+    var answer = 0;
+    let stack=[];
+    for (let i=0;i<ingredient.length;i++) {
+        stack.push(ingredient[i]);
+        
+        if (stack.length>=4) {
+            const burger=stack.slice(-4).join("");
+            
+            if (burger==="1231") {
+                stack.splice(-4);
+                answer+=1;
+            }
+        } 
+    }
+    return answer;
+}
