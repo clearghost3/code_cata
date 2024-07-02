85ff6269b63d0df9ac5f2cb826e49f6abe6f7b4d
clearghost3 <165910897+clearghost3@users.noreply.github.com>
2024-07-02 09:43:57 +0900

    Create 79.js

---
+++ 79.js
@@ -0,0 +1,17 @@
+function solution(brown, yellow) {
+    
+    let sum=brown+yellow;
+    
+    for (let height=3;height<=brown;height++) {
+        if (sum%height===0) {
+            let width=sum/height;
+            
+            if ((width-2)*(height-2)===yellow) {
+                return [width,height];
+            }
+        }
+    }
+    
+    var answer = [];
+    return answer;
+}
